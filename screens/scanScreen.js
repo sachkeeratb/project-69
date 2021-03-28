5e84ed5f0556de3fbed041e12640cb2b7a80504e
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2021-03-28 19:18:35 -0400

    Update scanScreen.js

--- screens/scanScreen.js
+++ screens/scanScreen.js
@@ -1,87 +1,89 @@
 import React from 'react';
-import { 
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-  Image,
-  StyleSheet,
-  KeyboardAvoidingView ,
-  ToastAndroid,
-  Alert
-} from 'react-native';
+import { Text, View, TouchableOpacity,Image, StyleSheet } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default class ScanScreen extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            hasCameraPermissions: null,
-            scanned: false,
-            scannedData: '',
-            buttonState: 'normal'
-        }
+  constructor(){
+    super();
+    this.state = {
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: '',
+      buttonState: 'normal'
     }
-    getCameraPermissions = async (id) =>{
-      const {status} = await Permissions.askAsync(Permissions.CAMERA);
-      
-      this.setState({
-        /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
-        */
-        hasCameraPermissions: status === "granted",
-        buttonState: id,
-        scanned: false
-      });
-    }
+  }
 
-    handleBarCodeScanned = async({type, data})=>{
-      const {buttonState} = this.state
+  getCameraPermissions = async () =>{
+    const {status} = await Permissions.askAsync(Permissions.CAMERA);  
+    this.setState({
+      hasCameraPermissions: status === "granted",
+      buttonState: 'clicked',
+      scanned: false
+    });
+  }
 
-      if(buttonState!="clicked"){
-        this.setState({
-          scanned: true,
-          scannedBookId: data,
-          buttonState: 'normal'
-        });
-      }
-      else if(buttonState==="clicked"){
-        this.setState({
-          scanned: true,
-          scannedStudentID: data,
-          buttonState: 'clicked'
-        });
-      }
-    }
+  handleBarCodeScanned = async({type, data})=>{
+    this.setState({
+      scanned: true,
+      scannedData: data,
+      buttonState: 'normal'
+    });
+  }
 
-    render(){
-        return(
-            <View>
-                <TouchableOpacity
-                onPress={this.getCameraPermissions}
-                style={StyleSheet.scanButton}
-                title="Bar Code Scanner">
-                    <Text style={styles.buttonText}>Scan QR Code</Text>
-                </TouchableOpacity>
-            </View>
-        );
+  render() {
+    const hasCameraPermissions = this.state.hasCameraPermissions;
+    const scanned = this.state.scanned;
+    const buttonState = this.state.buttonState;
+    if (buttonState === "clicked" && hasCameraPermissions){
+      return(
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
     }
+    else if (buttonState === "normal"){
+      return(
+        <View style={styles.container}>
+           <View>
+            <Image
+            source={require("../assets/camera.png")}
+              style={{width:200, height: 200}}/>
+            <Text style={{textAlign: 'center', fontSize: 30}}>Bar Code Scanner</Text>
+          </View>
+        <Text style={styles.displayText}>{
+          hasCameraPermissions===true ? this.state.scannedData: "Request Camera Permission"
+        }</Text>     
+
+        <TouchableOpacity
+          onPress={this.getCameraPermissions}
+          style= {styles.scanButton} 
+          title = "Bar Code Scanner">
+          <Text style={styles.buttonText}>Scan QR Code</Text>
+        </TouchableOpacity>
+      </View>
+      );
+    }
+  }
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    scanButton:{
-      backgroundColor: '#2196F3',
-      padding: 10,
-      margin: 10
-    },
-    buttonText:{
-        fontSize: 15,
-        textAlign: 'center',
-        marginTop: 10
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  displayText:{
+    fontSize: 15,
+    textDecorationLine: 'underline'
+  },
+  scanButton:{
+    backgroundColor: '#2196F3',
+    padding: 10,
+    margin: 10
+  },
+  buttonText:{
+    fontSize: 20,
+  }
 });
