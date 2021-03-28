cc2674541859f066c4b1cc1da0d4be8d547592aa
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2021-03-28 19:18:05 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,44 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { StyleSheet, Text, View } from 'react-native';
 
-import ScanScreen from './screens/scanScreen.js';
+
+import ScanScreen from './screens/scanScreen';
+
 
 export default class App extends React.Component {
   render(){
     return (
-      
-        <AppContainer />
-      
+      <ScanScreen />
     );
   }
 }
-
-const TabNavigator = createBottomTabNavigator({
-  Transaction: {screen: TransactionScreen},
-  Search: {screen: SearchScreen},
-},
-{
-  defaultNavigationOptions: ({navigation})=>({
-    tabBarIcon: ()=>{
-      const routeName = navigation.state.routeName;
-      console.log(routeName)
-      if(routeName === "scanScreen"){   
-      }
-      else if(routeName === "scanScreen"){        
-      }
-    }
-  })
-}
-);
-
-const SwitchNavigator = createSwitchNavigator({
-  LoginScreen: {screen: LoginScreen},
-  TabNavigator: {screen: TabNavigator},
-})
-
-const AppContainer =  createAppContainer(SwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
